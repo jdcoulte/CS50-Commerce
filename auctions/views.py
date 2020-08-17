b252c1b15af00fa9a4917047823ff50bee1cc37e
Jeff Coulter <jeff.coulter1@gmail.com>
2020-08-17 11:48:47 -0700

    minor changes

--- auctions/views.py
+++ auctions/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
-from .models import User, Listing, Bid, Comment
+from .models import User, Listing, Bid
 from .forms import ListingForm, BidForm
 
 import datetime
